82de7e3683543492969999d634ba8b71b626476c
sahishnu shrestha <sahishnushrestha9c@gmail.com>
2024-08-22 15:54:24 +0545

    run

---
+++ src/LoginScreen.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./LoginScreen.css";
+
+const LoginScreen = () => {
+  return (
+    <div className="login-screen">
+      <img
+        src={require("./assets/images/Vector 1-2.png")}
+        className="image1"
+        alt="Background"
+      />
+      <div className="logo">
+        <h1>LOGO</h1>
+      </div>
+      <div className="login-container">
+        <div className="left-panel">
+          <h1>LOREM IPSUM</h1>
+          <h2>Instructions:</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Egestas eu volutpat potenti
+            tellus viverra ultrices facilisis arcu. 
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+            <li>
+              Egestas eu volutpat potenti tellus viverra ultrices facilisis
+              arcu.
+            </li>
+          </ul>
+        </div>
+        <div className="right-panel">
+          <h2>Sign in to your account</h2>
+          <form>
+            <label>Email:</label>
+            <input type="email" placeholder="example@gmail.com" required />
+            <label>Password:</label>
+            <input type="password" placeholder="Enter your password" required />
+            <div className="recovery">
+              <div className="remember-me">
+                <input type="checkbox" id="rememberMe" />
+                <label htmlFor="rememberMe">Remember me</label>
+              </div>
+              <a href="#">Forgot your password?</a>
+            </div>
+            <button type="submit">Sign In</button>
+          </form>
+        </div>
+      </div>
+      <img
+        src={require("./assets/images/Students.png")}
+        className="image2"
+        alt="students"
+      />
+      <div class="circle">
+        <p class="text">
+          Registor <br></br>Today!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
---
+++ src/pages/RegistrationScreen.js
@@ -0,0 +1,212 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    organizationType: "",
+    country: "",
+    countryCode: "",
+    state: "",
+    organizationName: "",
+    address: "",
+    postalCode: "",
+    city: "",
+    email: "",
+    contactNumber: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const handleClear = () => {
+    setFormData({
+      organizationType: "",
+      country: "",
+      countryCode: "",
+      state: "",
+      organizationName: "",
+      address: "",
+      postalCode: "",
+      city: "",
+      email: "",
+      contactNumber: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
+  return (
+    <div className="body">
+      <img
+        src={require("./assets/images/Vectorimg22 (1).png")}
+        className="image1"
+        alt="Background1"
+      />
+      <div className="parent-container">
+        <div className="registration-container">
+          <h1>Register Your Organization Today</h1>
+          <h2>Country Information*</h2>
+          <form onSubmit={handleSubmit} className="registration-form">
+            <div className="grid-container">
+              <div className="form-group">
+                <label>Organization Type</label>
+                <select
+                  name="organizationType"
+                  onChange={handleChange}
+                  value={formData.organizationType}
+                >
+                  <option value="">Select</option>
+                  <option value="College">College</option>
+                  <option value="School">School</option>
+                </select>
+              </div>
+
+              <div className="form-group">
+                <label>Organization Name</label>
+                <input
+                  type="text"
+                  name="organizationName"
+                  onChange={handleChange}
+                  value={formData.organizationName}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Country</label>
+                <input
+                  type="text"
+                  name="country"
+                  onChange={handleChange}
+                  value={formData.country}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Country Code</label>
+                <input
+                  type="text"
+                  name="countryCode"
+                  onChange={handleChange}
+                  value={formData.countryCode}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Address</label>
+                <input
+                  type="text"
+                  name="address"
+                  onChange={handleChange}
+                  value={formData.address}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>City</label>
+                <input
+                  type="text"
+                  name="city"
+                  onChange={handleChange}
+                  value={formData.city}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>State/Province</label>
+                <input
+                  type="text"
+                  name="state"
+                  onChange={handleChange}
+                  value={formData.state}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Postal/Zip Code</label>
+                <input
+                  type="text"
+                  name="postalCode"
+                  onChange={handleChange}
+                  value={formData.postalCode}
+                />
+              </div>
+            </div>
+            <h2>Other Information*</h2>
+            <div className="grid-container2">
+              <div className="form-group">
+                <label>Email Address</label>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={formData.email}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Contact Number</label>
+                <input
+                  type="text"
+                  name="contactNumber"
+                  onChange={handleChange}
+                  value={formData.contactNumber}
+                />
+              </div>
+              <div></div>
+
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  value={formData.password}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Confirm Password</label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  onChange={handleChange}
+                  value={formData.confirmPassword}
+                />
+              </div>
+            </div>
+
+            <div className="form-buttons">
+              <button type="button" onClick={handleClear}>
+                Clear
+              </button>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+
+        <img
+          src={require("./assets/images/aStudent.png")}
+          className="image2"
+          alt="student"
+        />
+        <div className="circle">
+          <p className="text">Sign In</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegistrationForm;
